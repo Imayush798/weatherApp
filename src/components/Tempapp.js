19d8173d9210840eaa259582a04a9665710096cb
Imayush798 <ayushsahu798@gmail.com>
2023-04-30 15:55:05 +0530

    update

--- src/components/Tempapp.js
+++ src/components/Tempapp.js
@@ -4,7 +4,7 @@ import "./css/style.css";
 const Tempapp = () => {
 
     const [city, setCity]= useState(null);
-    const [search, setSearch] = useState("Prayagraj");
+    const [search, setSearch] = useState("manali");
 
     useEffect( () =>  {
         const fetchApi = async () => {
